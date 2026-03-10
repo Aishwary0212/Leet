5ed93471c0116f112e4d2840e583f05b2d33431c
Aishwary <aishwary0212@gmail.com>
2026-03-10 07:50:43 +0530

    updated links

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: ["http://localhost:5173", "http://localhost:5174"],
+        origin: ["http://localhost:5173", "http://localhost:5174", "https://leet-r609.onrender.com"],
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
     })
